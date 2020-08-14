6a86a836e5535f28b0fefb5a8f78a6f9a1f47a1d
Sophia <chiaoyin1520@gmail.com>
2020-08-15 02:39:27 +0800

    refactor : modify express routes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser')
 const URL = require('./models/URL.js')
 const mongoose = require('mongoose')
 const db = mongoose.connection
+const routes = require('./routes')
+
 
 mongoose.connect('mongodb://localhost/url-shortener-link', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -21,67 +23,9 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
+app.use(routes)
 
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
-
-app.post('/link', (req, res) => {
-  const userURL = req.body.URL
-  // console.log(userURL)
-  function getData() {
-    const data = "ABCDEFGHIJKLNMOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
-
-    let Random = ''
-    for (let i = 0; i < 5; i++) {
-      Random += data[Math.floor(Math.random() * data.length)]
-    }
-
-    // 防止有重覆的網址組合出現
-    let collected = []
-    if (collected.indexOf(Random) >= 0) {
-      return getData()
-    } else {
-      collected.push(Random)
-      return Random
-      // console.log(Random)
-      // console.log(collected)
-    }
-  }
-  getData()
-
-  URL.create({
-    name: userURL,
-    key: getData()
-  })
-    .then(item => res.render('show', { Random1: item.key, Random2: item.name, Random3: item._id }))
-    .catch(error => console.log(error))
-})
-
-
-
-
-app.get('/web/:id', (req, res) => {
-  console.log(req.params.id)
-  const id = req.params.id
-  URL.findById(id)
-    .lean()
-    .then(item => res.render('web', { web: item }))
-    .catch(error => console.log(error))
-})
-
-
-
-
-app.get('/:key', (req, res) => {
-  const key = req.params.key
-  console.log(req.params.key)
-  URL.findOne({ key: `${key}` })
-    .then(item => res.redirect(`${item.name}`))
-    .catch(error => console.log(error))
-})
 
 
 app.listen(PORT, () => {
---
+++ routes/modules/home.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const router = express.Router()
+const URL = require('../../models/URL.js')
+
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+
+router.post('/link', (req, res) => {
+  const userURL = req.body.URL
+  // console.log(userURL)
+  function getData() {
+    const data = "ABCDEFGHIJKLNMOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+
+    let Random = ''
+    for (let i = 0; i < 5; i++) {
+      Random += data[Math.floor(Math.random() * data.length)]
+    }
+
+    // 防止有重覆的網址組合出現
+    let collected = []
+    if (collected.indexOf(Random) >= 0) {
+      return getData()
+    } else {
+      collected.push(Random)
+      return Random
+      // console.log(Random)
+      // console.log(collected)
+    }
+  }
+  getData()
+
+  URL.create({
+    name: userURL,
+    key: getData()
+  })
+    .then(item => res.render('show', { Random1: item.key, Random2: item.name, Random3: item._id }))
+    .catch(error => console.log(error))
+})
+
+
+router.get('/web/:id', (req, res) => {
+  console.log(req.params.id)
+  const id = req.params.id
+  URL.findById(id)
+    .lean()
+    .then(item => res.render('web', { web: item }))
+    .catch(error => console.log(error))
+})
+
+
+
+
+
+router.get('/:key', (req, res) => {
+  const key = req.params.key
+  console.log(req.params.key)
+  URL.findOne({ key: `${key}` })
+    .then(item => res.redirect(`${item.name}`))
+    .catch(error => console.log(error))
+})
+
+
+module.exports = router
